e1b3a0a9b9352443d48f76493c6f643e0816dd5a
VaibhavKhushalani <vaibhavkhushlani04@gmail.com>
2022-01-08 01:17:23 +0530

    Navbar Footer update

--- src/components/loggedInUser/UserBlogs.js
+++ src/components/loggedInUser/UserBlogs.js
@@ -12,11 +12,11 @@ import UserContext from "../../utils/userContext";
 import axios from "../../utils/axios";
 
 function UserBlogs() {
-  const [state, setState] = useState("activeBlogs");
+  const [state, setState] = useState("Active Blogs");
   const [sidebarShow, setSidebarShow] = useState(false);
   const { user, isLoading } = useContext(UserContext);
   const [userdata, setUserData] = useState(null);
-  const [draftBlogs, setDraftBlogs] = useState(true);
+  const [draftBlogs, setDraftBlogs] = useState(false);
   const navigate = useNavigate();
 
   const Data = () => {
@@ -34,89 +34,95 @@ function UserBlogs() {
 
   useEffect(() => {
     Data();
-  }, []);
-  console.log(userdata);
+    if (state === "Active Blogs") {
+      setDraftBlogs(true);
+    } else {
+      setDraftBlogs(false);
+    }
+  }, [state]);
+  console.log(userdata?.data);
 
   if (!isLoading && !user) navigate("/");
   if (isLoading) return null;
-
-  return (
-    <>
-      <Header setSidebarShow={setSidebarShow} sidebarShow={sidebarShow} />
-      {sidebarShow && <Sidebar selected={"blogs"} />}
-      <Container className="userb-main">
-        <div className="userb-head">
-          <div className="userb-blogs">
-            <div
-              className={`${state === "activeBlogs" && "userb-active"}`}
-              onClick={() => setState("activeBlogs")}
-            >
-              Active Blogs
+  else {
+    return (
+      <>
+        <Header setSidebarShow={setSidebarShow} sidebarShow={sidebarShow} />
+        {sidebarShow && <Sidebar selected={"blogs"} />}
+        <Container className="userb-main">
+          <div className="userb-head">
+            <div className="userb-blogs">
+              <div
+                className={`${state === "Active Blogs" && "userb-active"}`}
+                onClick={() => {
+                  setState("Active Blogs");
+                }}
+              >
+                Active Blogs
+              </div>
+              <div
+                className={`${state === "Drafts" && "userb-active"}`}
+                onClick={() => {
+                  setState("Drafts");
+                }}
+              >
+                Drafts
+              </div>
             </div>
-            <div
-              className={`${state === "drafts" && "userb-active"}`}
-              onClick={() => setState("drafts")}
+            <button
+              className="userb-pub"
+              onClick={() => navigate("/user/publish")}
             >
-              Drafts
-            </div>
+              Publish
+            </button>
           </div>
-          <button
-            className="userb-pub"
-            onClick={() => navigate("/user/publish")}
-          >
-            Publish
-          </button>
-        </div>
-        {state === "activeBlogs" ? (
-          // userdata.map((data, index) => {
-           <div className="userb-ablog">
-                <div className="userb-abhead">Active Blogs</div>
-                <div className="userb-blog">
-                  <img src={Blog} alt="blog" />
-                  <div className="userb-blogc">
-                    <div className="userb-blogt">Topic</div>
-                    <div className="userb-blogb">
-                      
+          <div className="userb-ablog">
+            <p>{state}</p>
+
+            {userdata?.data.map((data, id) => {
+              if (state === "Active Blogs") {
+                return (
+                  <div className="Blog-box" key={id}>
+                    <img id="item-0" src={data?.thumbnail} alt="blog" />
+                    <div id="item-1">{data?.title}</div>
+                    <div id="item-2">
+                      {data?.blogText.slice(0, 150) + "..."}
+                      <span className="more-text">more</span>
                     </div>
-                    <div className="userb-blogi">
-                      <div className="userb-blogvs userb-pad">
-                        <img src={View} alt="view" />
-                        <span>102</span>
-                        <img src={Share} alt="share" />
-                        <span>14</span>
-                      </div>
-                      <div className="userb-blogcmt userb-pad">
-                        <img src={Comments} alt="comment" />
-                        <span>14</span>
-                      </div>
+                    <div id="item-3">
+                      <img src={View} alt="view" />
+                      <span> &nbsp; 102</span>
+                      <img className="item-share" src={Share} alt="share" />
+                      <span> &nbsp; 14</span>
+                      <span className="item-comments-span"> &nbsp; 17</span>
+                      <img
+                        className="item-comments"
+                        src={Comments}
+                        alt="comment"
+                      />
                     </div>
                   </div>
-                </div>
-              </div>
-           
-          // })
-        ) : (
-          <div className="userb-ablog">
-            <div className="userb-abhead">Drafts</div>
-            <div className="userb-blog">
-              <img src={Blog} alt="blog" />
-              <div className="userb-blogc">
-                <div className="userb-blogt">Topic</div>
-                <div className="userb-blogb">
-                 
-                </div>
-                <div className="userb-bloge">Edit</div>
-              </div>
-            </div>
+                );
+              } else {
+                return (
+                  <div className="Blog-box" key={id}>
+                    <img id="item-0" src={data?.thumbnail} alt="blog" />
+                    <div id="item-1">{data?.title}</div>
+                    <div id="item-2">
+                      {data?.blogText.slice(0, 150) + "..."}
+                      <span className="more-text">more</span>
+                    </div>
+
+                    <div id="item-3">Edit</div>
+                  </div>
+                );
+              }
+            })}
           </div>
-        )
-        
-        
-        
-        }
-      </Container>
-    </>
-  );
+        </Container>
+      </>
+    );
+  }
 }
 
 export default UserBlogs;
