630e57db237a32345bd6ff246c80c2b1485b8ca2
VaibhavKhushalani <vaibhavkhushlani04@gmail.com>
2022-02-06 12:18:00 +0530

    major changes

--- src/components/landing/StartCards.js
+++ src/components/landing/StartCards.js
@@ -1,22 +1,21 @@
 import React, { useState, useEffect } from "react";
 import "./StartCards.css";
 import { Container, Row, Col } from "react-bootstrap";
-import start1 from "../../assets/images/start1.svg";
-import start2 from "../../assets/images/start2.svg";
-import start3 from "../../assets/images/start3.svg";
-import start4 from "../../assets/images/start4.svg";
-import start5 from "../../assets/images/start5.svg";
+import { NavLink } from "react-router-dom";
+// import start1 from "../../assets/images/start1.svg";
+// import start2 from "../../assets/images/start2.svg";
+// import start3 from "../../assets/images/start3.svg";
+// import start4 from "../../assets/images/start4.svg";
+// import start5 from "../../assets/images/start5.svg";
 import axios from "../../utils/axios";
 function StartCards() {
   const [data, setData] = useState();
-
+  const [item1, setItem1] = useState();
   const GetData = () => {
     try {
-      axios
-        .get(`api/guest/promotedblogs`)
-        .then((response) => {
-          setData(response.data.data);
-        });
+      axios.get(`api/user/homepageblogs`).then((response) => {
+        setData(response.data.data);
+      });
     } catch (err) {
       console.log(err);
     }
@@ -24,68 +23,60 @@ function StartCards() {
   useEffect(() => {
     GetData();
   }, []);
-// console.log(data)
+  useEffect(() => {
+    if (data?.length > 0) {
+      setItem1(data[0]);
+    }
+  }, [data]);
   return (
     <Container className="start-main">
       <Row>
         <Col lg={7} className="start-col">
-          <img src={start1} alt="scene1" className="start-img1" />
+        <NavLink
+                className="navlink-css"
+                to="/particular-blog" state={{ blog_id: item1?._id }}
+              >
+          <div className="start-fit-img1">
+            <img src={item1?.thumbnail} alt="scene1" className="start-img1" />
+          </div>
           <div className="start-div">
-            <span className="start-category">Category</span>
-            <div className="start-heading">Lorem ipsum dolor sit amet</div>
+            <span className="start-category">{item1?.category}</span>
+            <div className="start-heading">{item1?.title}</div>
             <div className="start-subheading">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id nibh
-              ullamcorper nullam morbi eu fusce cursus. Felis commodo auctor
-              tempus torto.
+              {item1?.blogText.slice(0, 200)}...
             </div>
           </div>
+          </NavLink>
         </Col>
         <Col lg={5}>
           <Row>
-            <Col md={6} className="start-col">
-              <img src={start2} alt="scene2" className="start-img" />
-              <div className="start-sdiv">
-                <span className="start-scategory">Category</span>
-                <div className="start-sheading">Lorem ipsum dolor sit</div>
-                <div className="start-ssubheading">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id
-                  nibh ullamcorper
-                </div>
-              </div>
-            </Col>
-            <Col md={6} className="start-col">
-              <img src={start3} alt="scene3" className="start-img" />
-              <div className="start-sdiv">
-                <span className="start-scategory">Category</span>
-                <div className="start-sheading">Lorem ipsum dolor sit </div>
-                <div className="start-ssubheading">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id
-                  nibh ullamcorper
-                </div>
-              </div>
-            </Col>
-            <Col md={6} className="start-col">
-              <img src={start4} alt="scene4" className="start-img" />
-              <div className="start-sdiv">
-                <span className="start-scategory">Category</span>
-                <div className="start-sheading">Lorem ipsum dolor sit </div>
-                <div className="start-ssubheading">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id
-                  nibh ullamcorper
-                </div>
-              </div>
-            </Col>
-            <Col md={6} className="start-col">
-              <img src={start5} alt="scene5" className="start-img" />
-              <div className="start-sdiv">
-                <span className="start-scategory">Category</span>  
-                <div className="start-sheading">Lorem ipsum dolor sit </div>
-                <div className="start-ssubheading">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id
-                  nibh ullamcorper
-                </div>
-              </div>
-            </Col>
+            {data?.slice(1, 5).map((item, id) => {
+              return (
+                <>
+                  <Col md={6} className="start-col" key={id}>
+                  <NavLink
+                className="navlink-css"
+                to="/particular-blog" state={{ blog_id: item?._id }}
+              >
+                    <div className="start-fit-img">
+                      <img
+                        src={item?.thumbnail}
+                        alt="scene2"
+                        className="start-img"
+                      />
+                    </div>
+                    <div className="start-sdiv">
+                      <span className="start-scategory">{item?.category}</span>
+                      <div className="start-sheading">{item?.title}</div>
+                      <div className="start-ssubheading">
+                        {item?.blogText.slice(0, 150)}...
+                      </div>
+                    </div>
+                    </NavLink>
+                  </Col>
+                </>
+              );
+            })}
           </Row>
         </Col>
       </Row>
